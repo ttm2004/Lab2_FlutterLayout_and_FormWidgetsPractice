903f20b2a7622446aef0ccd49df82bd59735cda1
ttm2004 <manhtrong2k4@gmail.com>
2026-04-07 13:43:08 +0700

    Part1

---
+++ part1/android/app/src/main/kotlin/com/example/part1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.part1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ part1/lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: LayoutApp(),
+        ),
+    );
+  }
+}
+
+class LayoutApp extends StatelessWidget {
+  const LayoutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('I\'m in a Column and Centered. The below is a row', textAlign: TextAlign.center),
+        // Text('I\'m in a Column and Centered. The below is a row'),
+        const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            for (var color in [Colors.red, Colors.green, Colors.blue])
+              Container(width: 100, height: 100, color: color),
+          ],
+        ),
+        const SizedBox(height: 20),
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              width: 300,
+              height: 200,
+              color: Colors.yellow,
+            ),
+            const Text(
+              'Stacked on Yellow Box',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
